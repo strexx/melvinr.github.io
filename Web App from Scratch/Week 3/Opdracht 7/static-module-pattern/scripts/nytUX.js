27e5002ba11546f09e5ab120f075f02e0f8cd4d4
melvinr <melreijnoudt@gmail.com>
2016-02-19 10:06:59 +0100

    Added no-touch

--- Web App from Scratch/Week 3/Opdracht 7/static-module-pattern/scripts/nytUX.js
+++ Web App from Scratch/Week 3/Opdracht 7/static-module-pattern/scripts/nytUX.js
@@ -7,13 +7,16 @@ NYT.ux = (function () {
     var _nytBody = document.querySelector('body');
     var _mc = new Hammer(_nytBody);
 
-    function swipeBack() {
-        _mc.on('swiperight', (ev) => {
-            window.history.back();
-        });
+    function myGestures() {
+
+        function swipeBack() {
+            _mc.on('swiperight', (ev) => {
+                window.history.back();
+            });
+        }
     }
 
     return {
-        gestures: swipeBack
+        gestures: myGestures
     }
 })();
