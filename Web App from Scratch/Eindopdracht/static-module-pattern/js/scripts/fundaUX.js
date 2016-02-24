80103f9f635f4c12fb4c56853a53240f490ce7fb
melvinr <melreijnoudt@gmail.com>
2016-02-24 11:03:08 +0100

    added loader again

--- Web App from Scratch/Eindopdracht/static-module-pattern/js/scripts/fundaUX.js
+++ Web App from Scratch/Eindopdracht/static-module-pattern/js/scripts/fundaUX.js
@@ -5,16 +5,8 @@ var fShaker = fShaker || {};
 fShaker.ux = (function () {
 
     var _fShakerBody = document.querySelector('body');
-    var _mc = new Hammer(_fShakerBody);
     var _loadie = document.getElementById('loader');
-
-
-    function swipeBack() {
-        _mc.on('swiperight', (ev) => {
-            window.history.back();
-        });
-    }
-
+    
     function shake() {
         var myShakeEvent = new Shake({
             threshold: 15, // optional shake strength threshold
@@ -32,17 +24,16 @@ fShaker.ux = (function () {
     }
 
     //    based on the advice of Lisa Klein
-//        function loader(active) {
-//            if (active) {
-//                _loadie.classList.remove('inactive');
-//            } else {
-//                _loadie.classList.add('inactive');
-//            }
-//        }
+        function loader(active) {
+            if (active) {
+                _loadie.classList.remove('inactive');
+            } else {
+                _loadie.classList.add('inactive');
+            }
+        }
 
     return {
-        gestures: swipeBack,
-        shake: shake
-//        loader: loader
+        shake: shake,
+        loader: loader
     }
 })();
