d166d1feadb7cfa30b1595b0533d4810d07ec099
melvinr <melreijnoudt@gmail.com>
2016-03-22 12:37:33 +0100

    Created start for testing html5 notification

---
+++ Browser Technologies/Week 3/js/script.js
@@ -0,0 +1,47 @@
+var fn = fn || {};
+'use strict';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fn.launcher.init();
+});
+
+
+
+
+fn.launcher = (function () {
+    var init = function() {
+        fn.notification.init();
+    }
+    
+    return {
+        init: init
+    }
+})();
+
+fn.notification = (function() {
+    var askPermission =  function() {
+        if (!Notification in window) {
+            alert("hoi");
+        } else {
+            Notification.requestPermission(function (permission) {
+                if(permission === 'granted') {
+                    var notification = new Notification("hoi")
+                }
+            })
+        }
+    }
+    
+    return {
+        init: askPermission
+    }
+})();
+
+//
+//
+//var winMessage = "Leicester City have beaten Manchester City with a score of 1-3."
+//    
+//    setTimeout(function() {
+//        alert("Hoi");
+//    }, 3000);
+
