179b26673ff819056bcc40a765b32660f79e1d86
melvinr <melreijnoudt@gmail.com>
2016-03-23 13:52:38 +0100

    Fixed notification support check

--- Browser Technologies/Week 3/js/script.js
+++ Browser Technologies/Week 3/js/script.js
@@ -4,84 +4,100 @@ var fn = fn || {};
 
 var results = document.getElementById('results');
 var cluboption = document.getElementById('cluboption');
+var leicester = document.getElementById('leicester');
+var leicesterTwo = document.getElementById('leicestertwo');
+var docBody = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', function () {
-    results.classList.add('inactive');
-    cluboption.classList.remove('inactive');
-    askIt();
-    
+    console.log(isNewNotificationSupported());
+    if (isNewNotificationSupported()) {
+        docBody.classList.add('nf-supported');
+        fn.launcher.init();
+    } else {
+        fn.launcher.init();
+    }
+ 
 });
-
-
-
-function askIt() {
-    if (window.Notification && Notification.permission == 'granted') {
-        var notification = new Notification("hoi");
-    } else if (isNewNotificationSupported()) {
-        Notification.requestPermission();
-    }
-}
-
-function isNewNotificationSupported() {
-    if (!window.Notification || !Notification.requestPermission)
-        return false;
-    if (Notification.permission == 'granted')
-        throw new Error('');
-        alert("test 1");
-    try {
-        alert("test 2");
-        new Notification('');
-        alert("test 3");
-    } catch (e) {
-        if (e.name == 'TypeError')
-            fn.launcher.showSection();
-        return false;
-    }
-    return true;
-}
-
 
 fn.launcher = (function () {
     var init = function () {
-        fn.notification.init();
-    }
-
-    var pushNotification = document.getElementById('push-notification');
-
-    var showSection = function () {
-        pushNotification.classList.remove('inactive');
+        if (docBody.className === 'nf-supported') {
+            fn.notification.webNotification();
+        } else {
+            fn.notification.showSection();
+        }
     }
 
     return {
-        init: init,
-        showSection: showSection
+        init: init
     }
 })();
 
+
+fn.notification = (function () {
+    var pushNotification = document.getElementById('push-notification');
+    function showSection() {
+        pushNotification.classList.remove('inactive');
+    };
+
+    function webNotification() {
+        if (window.Notification && Notification.permission == 'granted') {
+            var notification = new Notification(leicester.innerHTML);
+        } else if (isNewNotificationSupported()) {
+            Notification.requestPermission();
+        }
+    }
+
+    return {
+        showSection: showSection,
+        webNotification: webNotification
+    }
+
+})();
+
+
+//Thanks to Joost Faber for helping me rewrite this for progressive enhancement.
+//Source: https://developers.google.com/web/updates/2015/05/notifying-you-of-changes-to-notifications
+function isNewNotificationSupported() {
+    var docBody = document.querySelector('body');
+    if (!window.Notification || !Notification.requestPermission) {
+        return false;
+    }
+        
+    if (Notification.permission == 'granted'){
+        docBody.classList.add('nf-supported');
+        return true;
+    }
+    try {
+        new Notification('...');
+    } catch (e) {
+        if (e.name == 'TypeError')
+            fn.notification.showSection();
+        return false;
+    }
+    
+    return true;
+}
+
 //
-//fn.notification = (function () {
-//    var askPermission = function () {
-//        if (!Notification in window) {
-//            alert("hoi");
-//        } else {
-//            Notification.requestPermission(function (permission) {
-//                if (permission === 'granted') {
-//                    var bla;
-//                    var notification = new Notification("hoi");
-//                }
-//            })
-//        }
+//fn.launcher = (function () {
+//    var init = function () {
+//        fn.notification.init();
 //    }
 //
+//    var pushNotification = document.getElementById('push-notification');
+//
+//
+//
 //    return {
-//        init: askPermission
+//        init: init,
+//        showSection: showSection
 //    }
 //})();
 
-//
-//
-//var winMessage = "Leicester City have beaten Manchester City with a score of 1-3."
-//    
-//    setTimeout(function() {
-//        alert("Hoi");
-//    }, 3000);
+
+
+if ('alert' in window) {
+    results.classList.add('inactive');
+    cluboption.classList.remove('inactive');
+}
