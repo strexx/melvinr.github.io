af54d80211cc0384a79ee5ad02b3718ebc04fe1f
melvinr <melvinr@MacBook-Pro-van-Melvin.local>
2016-04-07 22:42:41 +0200

    Edited code

--- WebofThings/motion-ldr/motion-ldr.ino
+++ WebofThings/motion-ldr/motion-ldr.ino
@@ -14,24 +14,23 @@
 #include <WiFiUdp.h>
 
 
-//pins and leds
+//Pins and leds with values
 int pinPir = D0;
 int pinLdr = A0;
-int pinLed = LED_BUILTIN;
 int pirState = LOW;
-int val = 0;  
+int val = 0;
 int light = 0;
 
 
-//Define client
-const char* ssid = "AllKindzzz";
-const char* password = "Boelers159753!";
+//Define client, use your wifi or hotspot's SSID and password
+const char* ssid = "";
+const char* password = "";
 WiFiClient client;
 
-//keys
+//Keys for EIoTCloudRestApi
 EIoTCloudRestApi eiotcloud;
-#define AP_SSID "AllKindzzz"
-#define AP_PASSWORD "Boelers159753!"
+#define AP_SSID ""
+#define AP_PASSWORD ""
 #define PIR_ID "57039c74c943a0661cf3149d/dLCRbLuCtg3NBJMP"
 #define LDR_ID "57039c74c943a0661cf3149d/je3efB3WsdCBwJEu"
 
@@ -39,9 +38,8 @@ EIoTCloudRestApi eiotcloud;
 unsigned long CHANNEL_ID = 106699;
 const char * API_KEY = "2QITX0H6ZFQ7RS3R";
 
-
+//Setup for use
 void setup() {
- pinMode(pinLed, OUTPUT);
  pinMode(pinPir, INPUT);
  Serial.begin(9600);
  eiotcloud.begin();
@@ -51,26 +49,30 @@ void setup() {
 void loop() {
   delay(1000);
 
+  //Get LDR value and send to cloud
   light = analogRead(pinLdr);
   Serial.println(light);
   eiotcloud.sendParameter(LDR_ID, light);
   ThingSpeak.writeField(CHANNEL_ID, 2, light, API_KEY);
-  
-  
+
+
+//Send pir sensor's values to cloud.
  val = digitalRead(pinPir);
+     //If the returned value is high and pirState is low, motion has been detected, this will be sent to the cloud
+     //and state will be set to high
     if (val == HIGH) {
         if (pirState == LOW) {
-          digitalWrite(pinLed, HIGH);
           Serial.println("Motion detected!");
-          ThingSpeak.writeField(CHANNEL_ID, 1, val, API_KEY);
+          ThingSpeak.writeField(CHANNEL_ID, 1, 1, API_KEY);
            eiotcloud.sendParameter(PIR_ID, 1);
           pirState = HIGH;
         }
     } else {
+      //If the value is low and the pirState is high, there is no motion, this will be sent to the cloud
+      //and state will be set to low.
         if (pirState == HIGH) {
-          digitalWrite(pinLed, LOW);
           Serial.println("Motion ended");
-          ThingSpeak.writeField(CHANNEL_ID, 1, val, API_KEY);
+          ThingSpeak.writeField(CHANNEL_ID, 1, 0, API_KEY);
            eiotcloud.sendParameter(PIR_ID, 0);
           pirState = LOW;
         }
